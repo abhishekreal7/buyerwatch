64ca589bb74de01d37bc987406d7b22b7c75ae92
Abhishek S B <abhishekreal79@gmail.com>
2026-08-04 20:56:14 +0530

    style(sidebar): add optical padding to CustomDashboardIcon viewBox to scale-match surrounding Heroicons

--- src/components/DashboardLayout.tsx
+++ src/components/DashboardLayout.tsx
@@ -48,7 +48,7 @@ export type DashboardBootstrap = {
 function CustomDashboardIcon({ className }: { className?: string }) {
   return (
     <svg
-      viewBox="280 180 452 448"
+      viewBox="235 135 542 538"
       className={className}
       xmlns="http://www.w3.org/2000/svg"
     >
